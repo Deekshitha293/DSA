200806d828a766217332d2557a89cc37b25b7664
Deekshitha <deekshithadeekshithab293@gmail.com>
2025-10-09 06:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (90.33%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        # Dictionary to store number -> index map
+        num_dict = {}
+
+        # Iterate over the array
+        for i, num in enumerate(nums):
+            complement = target - num  # what we need to find
+            if complement in num_dict:
+                # Found the pair, return their indices
+                return [num_dict[complement], i]
+            # Store current number and its index
+            num_dict[num] = i
+
+        # In case no solution is found (though question guarantees one)
+        return []
