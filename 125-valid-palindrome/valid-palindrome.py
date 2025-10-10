d2b6fff3645214b121226553442751c52a5c0bdd
Deekshitha <deekshithadeekshithab293@gmail.com>
2025-10-10 09:22:41 +0530

    Time: 19 ms (43.97%) | Memory: 14.1 MB (28.72%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,7 +3,6 @@ class Solution:
         # Normalize string: lowercase + keep only alphanumeric characters
         s = ''.join(char.lower() for char in s if char.isalnum())
         
-        # Two-pointer check
         i, j = 0, len(s) - 1
         while i < j:
             if s[i] != s[j]:
