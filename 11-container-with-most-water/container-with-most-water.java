12cb771726d8bfcab7e407f32a9009108c3a8750
Deekshitha <deekshithadeekshithab293@gmail.com>
2025-10-20 23:44:27 +0530

    Time: 4 ms (91.70%) | Memory: 57.9 MB (65.57%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -10,7 +10,7 @@ class Solution {
             int area = width * minHeight;
 
             if (area > maxArea) {
-                maxArea = area;
+                maxArea = area; 
             }
 
             
