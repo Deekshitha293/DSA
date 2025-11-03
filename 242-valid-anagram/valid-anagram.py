0ca5ddee2d06bbb2510e00ab4fc62ce524d1fe27
Deekshitha <deekshithadeekshithab293@gmail.com>
2025-11-03 23:21:25 +0530

    Time: 27 ms (25.57%) | Memory: 13.6 MB (22.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,5 +4,5 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        # Compare sorted versions of bs
+        # Compare sorted versions of 
         return sorted(s) == sorted(t)
