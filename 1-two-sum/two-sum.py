4f423f15cbbb1f1787dbabb5406d5e1993c0d04b
Deekshitha <deekshithadeekshithab293@gmail.com>
2025-11-21 23:15:12 +0530

    Time: 2 ms (59.41%) | Memory: 13 MB (76.93%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums, target):
-        # Dictionary to store number ->  
+        # Dictionary to store number 
         num_dict = {}
 
         # Iterate over the array
