c2099dafa373a716041e0234348a156473a255f4
Deekshitha <deekshithadeekshithab293@gmail.com>
2026-01-23 23:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (68.60%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def runningSum(self, nums):
+        running_sum = []
+        current_sum = 0
+        
+        for num in nums:
+            current_sum += num
+            running_sum.append(current_sum)
+        
+        return running_sum
