b5180b1b5ecf9490e766da92a852a55bef9b7021
Deekshitha <deekshithadeekshithab293@gmail.com>
2026-01-28 00:12:53 +0530

    Time: 3 ms (21.87%) | Memory: 12.6 MB (28.86%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for i in range(len(image)):
+            image[i]=image[i][::-1]
+            for j in range(len(image[i])):
+                image[i][j] = 1 - image[i][j]
+
+        return image
